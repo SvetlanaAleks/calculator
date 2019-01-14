8a308e1bf84759a1df21ca8f7ba44d73fa41a441
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2019-01-14 23:40:27 +0300

    add form and getDataForm

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
 //=require partials/app.js
+
+
+$(document).ready(function () {
+   App.init();
+});
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,37 @@
 //Some JS
+const App = (function () {
+   "use strict";
+   return {
+      validationForm: function () {
+         $(".js_input-num").keydown(function (event) {
+            // Разрешаем: backspace, delete, tab и escape
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+               // Разрешаем: Ctrl+A
+               (event.keyCode == 65 && event.ctrlKey === true) ||
+               // Разрешаем: home, end, влево, вправо
+               (event.keyCode >= 35 && event.keyCode <= 39)) {
+               // Ничего не делаем
+               return;
+            } else {
+               // Убеждаемся, что это цифра, и останавливаем событие keypress
+               if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+                  event.preventDefault();
+               }
+            }
+         });
+      },
+      getDataForm: function () {
+         $('#js_indicators-form').submit(function (e) {
+            const _this = $(this);
+            e.preventDefault();
+            const valuesArray = _this.serializeArray();
+            console.log(valuesArray);
+
+         });
+      },
+      init: function () {
+         App.validationForm();
+         App.getDataForm();
+      },
+   };
+})();
